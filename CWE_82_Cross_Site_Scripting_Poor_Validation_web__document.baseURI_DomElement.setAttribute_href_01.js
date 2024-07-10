7143245a84285b40ebe6de87ebf9a719738a1fd0
chenrongbiao <chenrongbiao@huawei.com>
2024-07-10 10:46:08 +0800

    Add files via upload

---
+++ CWE_82_Cross_Site_Scripting_Poor_Validation_web__document.baseURI_DomElement.setAttribute_href_01.js
@@ -0,0 +1,52 @@
+/**
+ * @description 最简单的数据流传递过程。
+ *
+ * @cwe 82
+ * @source document.baseURI
+ * @sink DomElement.setAttribute_href
+ * @bad bad
+ * @good good
+ * @tool fortify: Cross-Site Scripting: Poor Validation;secbrella: SecJS_Cross_Site_Scripting
+ * @author 周通 z30021988
+ */
+
+function bad() {
+    let data;
+
+    data = ''; /* Initialize data */
+
+    /* Read data from a document baseURI */
+    data = document.baseURI;
+
+
+    if (data !== null) {
+        let urlParsingNode = document.createElement('a');
+        /* POTENTIAL FLAW: 依靠 HTML、XML 或其他类型编码验证用户输入可能会导致浏览器执行恶意代码。 */
+        urlParsingNode.setAttribute('href', encodeURI(data));
+    }
+
+}
+
+function good() {
+    goodG2B();
+}
+
+function goodG2B() {
+    let data;
+
+    data = 'foo';
+
+    if (data !== null) {
+        let urlParsingNode = document.createElement('a');
+        /* POTENTIAL FLAW GOOD: 依靠 HTML、XML 或其他类型编码验证用户输入可能会导致浏览器执行恶意代码。 */
+        urlParsingNode.setAttribute('href', encodeURI(data));
+    }
+
+}
+
+function main() {
+    bad();
+    good();
+}
+
+main();
